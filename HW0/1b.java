0b4e3f60d0a49fa2f468df5a70f1a0be99f3677e
shzhang <shzhang@umd.edu>
2017-02-26 19:10:46 +0800

    Create 1b.java

---
+++ HW0/1b.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        drawTriangle(10);
+    }
+
+    public static void drawTriangle(int N) {
+        int row = 0;
+        int SIZE = N;
+        while (row <= SIZE) {
+            int col = 0;
+            while (col <= row) {
+                System.out.print('*');
+                col = col + 1;
+            }
+            System.out.println();
+            row = row + 1;
+        }
+    }
+}
