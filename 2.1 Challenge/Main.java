de0cc8cf1cc2f2850c760612be4dcd75329ea785
shzhang <shzhang@umd.edu>
2017-03-01 22:01:30 +0800

    Update Main.java

--- 2.1 Challenge/Main.java
+++ 2.1 Challenge/Main.java
@@ -1,29 +1,13 @@
 package com.company;
 
-public class IntList {
-
-    public int first;
-    public IntList rest;
-    public IntList(int f, IntList r) {
-    first = f;
-    rest = r;
-    }
-
-    public int size() {
-        if (rest == null) {
-            return 1;
-        }
-        return 1 + this.rest.size();
-    }
-
-    public int get(int i) {
-        int item = this.first;
-        IntList temp = this.rest;
-        while (i > 0) {
-            item = temp.first;
-            temp = temp.rest;
-            i = i - 1;
-        }
-        return item;
+public class Main {
+    public static void main(String[] args) {
+        IntList test = new IntList(40, null);
+        test = new IntList(30, test);
+        test = new IntList(20, test);
+        test = new IntList(10, test);
+        test = new IntList(0, test);
+        System.out.println(test.get(2));
+        System.out.println(test.size());
     }
 }
