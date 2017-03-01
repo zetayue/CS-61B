ae647fa6a760a255d8761fbc771123dc2ffe0233
shzhang <shzhang@umd.edu>
2017-03-01 22:24:25 +0800

    Update IntList.java

--- 2.1 Challenge/IntList.java
+++ 2.1 Challenge/IntList.java
@@ -1,12 +1,10 @@
-package com.company;
-
 public class IntList {
 
-    public int first;
-    public IntList rest;
+    int first;
+    IntList rest;
     public IntList(int f, IntList r) {
-    first = f;
-    rest = r;
+        first = f;
+        rest = r;
     }
 
     public int size() {
@@ -25,5 +23,15 @@ public class IntList {
             i = i - 1;
         }
         return item;
+    }
+
+    public static void main(String[] args) {
+        IntList test = new IntList(40, null);
+        test = new IntList(30, test);
+        test = new IntList(20, test);
+        test = new IntList(10, test);
+        test = new IntList(0, test);
+        System.out.println(test.get(2));
+        System.out.println(test.size());
     }
 }
